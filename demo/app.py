0d2db870b2d43c1ce99a3ce04be112809aaddaa6
Rahul Nene <rahul@garudaire.com>
2023-03-20 13:14:18 +0530

    .

--- demo/app.py
+++ demo/app.py
@@ -41,8 +41,8 @@ def upload_file():
                 flash(message=f'{file.filename} is of an invalid type.')
             elif file:
                 filename = secure_filename(file.filename)
-                subprocess.call('mkdir /home/gauserapp/openMVS/demo/{token}/uploads/images')
-                subprocess.call('mkdir /home/gauserapp/openMVS/demo/{token}/output')
+                subprocess.run('mkdir /home/gauserapp/openMVS/demo/{token}/uploads/images')
+                subprocess.run('mkdir /home/gauserapp/openMVS/demo/{token}/output')
                 file.save(os.path.join(path,token,'uploads/images',filename))
                 flash(message=f'{file.filename} uploaded successfully with token {token}.')
         return redirect('/')
