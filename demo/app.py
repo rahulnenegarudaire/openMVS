82c0098f2917dad83422d2e49d1afac36b53ade4
Rahul Nene <rahul@garudaire.com>
2023-03-20 13:53:40 +0530

    .

--- demo/app.py
+++ demo/app.py
@@ -41,7 +41,7 @@ def upload_file():
                 flash(message=f'{file.filename} is of an invalid type.')
             elif file:
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(os.path.join(UNIQUE_FOLDER,"uploads"),filename))
+                file.save(os.path.join(UNIQUE_FOLDER,"uploads",filename))
                 flash(message=f'{file.filename} uploaded successfully with token {token}.')
         return redirect('/')
 
