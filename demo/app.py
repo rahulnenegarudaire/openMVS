7a45b7a570e0bf4b34bcbe1e26d28b5603340e2d
Rahul Nene <rahul@garudaire.com>
2023-03-22 12:47:04 +0530

    clean up

--- demo/app.py
+++ demo/app.py
@@ -29,12 +29,9 @@ def upload_form():
 def upload_file():
     
     UNIQUE_FOLDER = os.path.join(path,TOKEN)
-    # subprocess.run(f"mkdir {UNIQUE_FOLDER}", shell=True)
     os.mkdir(f"{UNIQUE_FOLDER}")
     os.mkdir(os.path.join(f"{UNIQUE_FOLDER}","uploads"))
     os.mkdir(os.path.join(f"{UNIQUE_FOLDER}","output"))
-    # subprocess.run(f"mkdir os.path.join({UNIQUE_FOLDER},uploads)", shell=True)
-    # subprocess.run(f"mkdir os.path.join({UNIQUE_FOLDER},output)", shell=True)
     if request.method == 'POST':
         if 'files[]' not in request.files:
             flash('No valid files selected.')
@@ -44,7 +41,6 @@ def upload_file():
             if not allowed_file(file.filename):
                 flash(message=f'{file.filename} is of an invalid type.')
             elif file:
-                # filename = secure_filename(file.filename)
                 file.save(os.path.join(UNIQUE_FOLDER,"uploads",file.filename))
                 flash(message=f'{file.filename} uploaded successfully with token {TOKEN}.')
         return redirect('/')
